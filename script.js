e02c2ca3f1496a326fac2d8d3c591d64c853d038
Christian Ramirez <christian.r0602@gmail.com>
2022-06-28 12:12:38 -0500

    class extension

--- script.js
+++ script.js
@@ -13,9 +13,27 @@ class Ninja {
   }
   drinkShake(){
     this.health += 10
+    console.log(`Ninja is drinking his protein shake to start his recovery after a grueling session of training, health is now ${this.health}`)
   }
 }
 
-const michaelAngelo = new Ninja('Michael Angelo', 10)
-michaelAngelo.drinkShake()
-console.log(michaelAngelo.showStats())
+
+// Extending Ninja class to create sensei class
+
+class Sensei extends Ninja {
+  constructor(name, health, speed, strength, wisdom){
+    super(name, health= 200, speed = 10, strength=10, wisdom = 10);
+    this.wisdom = wisdom
+  }
+  speakWisdom(){
+    this.drinkShake()
+    console.log('When you have exhausted all possibilities, remember this - you have not')
+  }
+}
+
+// const michaelAngelo = new Ninja('Michael Angelo', 10)
+// michaelAngelo.drinkShake()
+// console.log(michaelAngelo.showStats())
+
+const chiefSensei = new Sensei('Christian Ramirez')
+console.log(chiefSensei.speakWisdom())
